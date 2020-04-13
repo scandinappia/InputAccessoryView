02e17c988f91dde20208bc69d43994b15565e0d6
kamil.piestrzynski-at-690102928222 <kamil.piestrzynski@ikano.se>
2020-04-13 11:48:49 +0200

    Initial Commit

---
+++ InputAccessoryView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InputAccessoryView
+//
+//  Created by Kamil Piestrzynski on 2020-04-13.
+//  Copyright © 2020 scandinappia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
